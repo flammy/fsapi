9e905d0b52e8c0f7c78867bd9c9602370f1d6ffd
Lukas Rademacher <lukas@rademacher.ac>
2017-06-30 21:28:14 +0200

    removed loggers

--- src/Loggers/LogEcho.php
+++
@@ -1,7 +0,0 @@
-<?php
-class LogEcho extends Logger implements  Loggers
-{
-    public function log($level, $message, array $context = array()){
-        echo $this->addDate($message);
-    }
-}
--- src/Loggers/LogReturn.php
+++
@@ -1,7 +0,0 @@
-<?php
-class LogReturn extends Logger implements  Loggers
-{
-    public function log($level, $message, array $context = array()){
-        return $this->addDate($message);
-    }
-}
